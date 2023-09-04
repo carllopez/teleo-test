7477edbe5ef9664e8b8ef3dbfa5de8744d1e139d
Carlos López <carl.lopez@gmail.com>
2023-09-04 17:14:34 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshots: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ screenshots: json }))
+  }
+
+  render() {
+    const { screenshots } = this.state;
+    return (
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">User screenshots</h1>
+        </div>
+        {screenshots.map((screenshot) => (
+          <div className="card" key={screenshot.id}>
+            <div className="card-header">
+              #{screenshot.id} <strong>{screenshot.title}</strong>
+            </div>
+            <div className="card-body">
+              <p className="card-text"><img src={screenshot.url}></img></p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
